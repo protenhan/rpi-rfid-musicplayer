f6556080c43cf77536bf3eb281c8c02317170651
Phillipp von Rotenhan <phillipp.von.rotenhan@gmail.com>
2018-02-07 10:23:49 +0100

    added tagging and pushing of "latest" image

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 image="protenhan/rpi-rfid-musicplayer"
+docker tag rpi-rfid-musicplayer "$image:latest"
+docker push "$image:latest"
 docker tag rpi-rfid-musicplayer "$image:$TRAVIS_TAG-alpine-$ARCH"
 docker push "$image:$TRAVIS_TAG-alpine-$ARCH"
-docker push "$image:latest"
-
