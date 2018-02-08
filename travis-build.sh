e6d6aa717da62eb90e9a438d363c97f8edaa82d5
Phillipp von Rotenhan <protenhan@users.noreply.github.com>
2018-02-08 05:32:33 +0100

    Feature/ci setup (#1)
    * removed additional version printout
    
    * now statically linking go binary because of glibc vs musl libc issue

--- travis-build.sh
+++ travis-build.sh
@@ -6,11 +6,9 @@ uname -a
 echo "Updating Docker engine to have multi-stage builds"
 sudo service docker stop
 curl -fsSL https://get.docker.com/ | sudo sh
-docker version
 
 go get github.com/karalabe/xgo
-xgo -go 1.9.2 --targets=linux/arm-* github.com/protenhan/rpi-rfid-musicplayer
-ls -la rpi-rfid*
+xgo -go 1.9.2 --targets=linux/arm-7 -ldflags "-linkmode external -extldflags -static" github.com/protenhan/rpi-rfid-musicplayer
 file rpi-rfid-musicplayer-linux-arm-7
 
 if [ -d tmp ]; then
