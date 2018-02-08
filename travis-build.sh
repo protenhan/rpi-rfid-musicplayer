b8ef3972f64587fe5e275eb411f2f9fc352b9aa1
Phillipp von Rotenhan <phillipp.von.rotenhan@gmail.com>
2018-02-08 04:59:06 +0100

    removed additional version printout

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,6 @@ uname -a
 echo "Updating Docker engine to have multi-stage builds"
 sudo service docker stop
 curl -fsSL https://get.docker.com/ | sudo sh
-docker version
 
 go get github.com/karalabe/xgo
 xgo -go 1.9.2 --targets=linux/arm-* github.com/protenhan/rpi-rfid-musicplayer
