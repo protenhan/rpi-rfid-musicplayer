13b4dfdcb0611d0bf975568901f16ed5e5aed843
Phillipp von Rotenhan <phillipp.von.rotenhan@gmail.com>
2018-02-08 04:45:48 +0100

    building multiple binaries

--- travis-build.sh
+++ travis-build.sh
@@ -9,7 +9,8 @@ curl -fsSL https://get.docker.com/ | sudo sh
 docker version
 
 go get github.com/karalabe/xgo
-xgo -go 1.9.2 --targets=linux/arm-7 github.com/protenhan/rpi-rfid-musicplayer
+xgo -go 1.9.2 --targets=linux/arm-* github.com/protenhan/rpi-rfid-musicplayer
+ls -la rpi-rfid*
 file rpi-rfid-musicplayer-linux-arm-7
 
 if [ -d tmp ]; then
