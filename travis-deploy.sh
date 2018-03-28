3ceec82750ec9ca3b9d5a0c435c471dbf077b1ce
Phillipp von Rotenhan <phillipp.von.rotenhan@gmail.com>
2018-03-28 09:34:44 +0200

    Now always pushing a latest image

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,7 +2,13 @@
 set -e
 
 image="protenhan/rpi-rfid-musicplayer"
+# push tagged images if the commit was tagged
+if [ $# -eq 1 ]; then
+  docker tag rpi-rfid-musicplayer "$image:$TRAVIS_TAG-alpine-$ARCH"
+  docker push "$image:$TRAVIS_TAG-alpine-$ARCH"
+fi
+ 
+# always push a latest image
 docker tag rpi-rfid-musicplayer "$image:latest"
 docker push "$image:latest"
-docker tag rpi-rfid-musicplayer "$image:$TRAVIS_TAG-alpine-$ARCH"
-docker push "$image:$TRAVIS_TAG-alpine-$ARCH"
+  
