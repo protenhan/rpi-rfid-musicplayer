d6c7dc93bcad50a8c5a75d5bf8859d10f0b88411
Phillipp von Rotenhan <phillipp.von.rotenhan@gmail.com>
2018-12-15 20:39:21 +0100

    redirecting stderr of mpv player to /dev/null

--- src/player/start_player.sh
+++ src/player/start_player.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start mpv player in idle, set the json ipc socket path and fork the process
-mpv --idle --no-video --input-ipc-server=/tmp/mpvsocket & 
+mpv --idle --no-video --input-ipc-server=/tmp/mpvsocket 2>/dev/null & 
 
 # wait 2 seconds for mpv to start (we are running on a raspberry...)
 sleep 2
